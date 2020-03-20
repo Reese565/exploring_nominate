4be2c788918fe423e71f5091dbcbb7c59a2348cc
Maurice Williams <maurice.t.williams@gmail.com>
2020-03-20 15:21:33 -0700

    commit changes

--- flask-app.py
+++ flask-app.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import io
+import json
 
 import pandas as pd
 import numpy as np
@@ -19,33 +20,40 @@ cong_nom_mu = pd.read_csv('static/data/congq_nom_mu.csv')
 calif_url = """
 https://github.com/JeffreyBLewis/congressional-district-boundaries/raw/master/California_108_to_112.geojson?print=pretty
 """
-r = requests.get(calif_url)
+# r = requests.get(calif_url)
 
-sf = None
-jeff = None
+# sf = None
+# jeff = None
 
-for dist in r.json()['features']:
-    d = dist['properties']['district']
-    if d == '12':
-        sf = dist
-    elif d == '1':
-        jeff = dist
+# for dist in r.json()['features']:
+#     d = dist['properties']['district']
+#     if d == '12':
+#         sf = dist
+#     elif d == '1':
+#         jeff = dist
 
-dist_geoJSON = {
-  "type": "FeatureCollection",
-  "features": [sf, jeff]
-  }
+# dist_geoJSON = {
+#   "type": "FeatureCollection",
+#   "features": [sf, jeff]
+#   }
 
-del r        
+# del r        
 
-district_dist = np.arange(1,436)/435
+indx = np.arange(0,435)
+district_dist = pd.DataFrame({'index':indx, 'q':indx/435,'row':indx%29,'col':indx//29}).to_json(orient='records')
+nom = pd.Series(np.arange(-1, 1, .01)).to_json()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/getDists')
-def getDists():
+@app.route('/getData')
+def getData():
     # return data.to_json(orient="records")
-    return dist_geoJSON
+    return district_dist
+
+@app.route('/getNom')
+def getNom():
+    # return data.to_json(orient="records")
+    return nom
 
